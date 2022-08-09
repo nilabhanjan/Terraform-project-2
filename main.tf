03d3e37a761e78a23f5e8840bea0f52df82bd6e9
NILABH <nilavanjanchutia@gmail.com>
2022-08-09 12:29:20 +0530

    end

--- main.tf
+++ main.tf
@@ -35,10 +35,14 @@ resource "aws_route_table" "prod-route-table" {
   }
 }
 
+variable "subnet_prefix" {
+    description = "cidr block for subnet"
+  
+}
 
 resource "aws_subnet" "subnet-1" {
     vpc_id = aws_vpc.prod-vpc.id
-    cidr_block = "10.0.1.0/24"
+    cidr_block = var.subnet_prefix
     availability_zone= "us-east-1a"
 
     tags ={
@@ -134,4 +138,10 @@ resource "aws_instance" "web-server-instance" {
      tags ={
         Name ="web-server"
      }           
+}
+
+
+output "server_public_ip" {
+    value = aws_eip.one.public_ip
+  
 }
